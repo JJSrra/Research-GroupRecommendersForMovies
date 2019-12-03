51bc5f1c7d70df858e3f7323e63c3472cc7d4d1c
Juanjo Sierra <jjsierra103@gmail.com>
2019-12-03 17:19:13 +0100

    Improved ratings by removing 0.0 individual ratings from combination strategies

--- combination_strategies.py
+++ combination_strategies.py
@@ -2,13 +2,21 @@ import numpy as np
 from collections import Counter
 
 def avg(ratings):
-    return np.array(ratings).mean()
+    # Remove 0.0 ratings
+    ratings = np.setdiff1d(ratings, 0.0)
+    return 0.0 if len(ratings) == 0 else np.array(ratings).mean()
 
 def max(ratings):
-    return np.array(ratings).max()
+    # Remove 0.0 ratings
+    ratings = np.setdiff1d(ratings, 0.0)
+    return 0.0 if len(ratings) == 0 else np.array(ratings).max()
 
 def min(ratings):
-    return np.array(ratings).min()
+    # Remove 0.0 ratings
+    ratings = np.setdiff1d(ratings, 0.0)
+    return 0.0 if len(ratings) == 0 else np.array(ratings).min()
 
 def maj(ratings):
-    return Counter(ratings).most_common()[0][0]
+    # Remove 0.0 ratings
+    ratings = np.setdiff1d(ratings, 0.0)
+    return 0.0 if len(ratings) == 0 else Counter(ratings).most_common()[0][0]
