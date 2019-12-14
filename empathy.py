41db0afbf7522ebeff8a376505bd858af00f14c1
Juanjo Sierra <jjsierra103@gmail.com>
2019-12-14 13:10:24 +0100

    Created movie selection for Empathy

--- empathy.py
+++ empathy.py
@@ -2,12 +2,18 @@ import numpy as np
 import evaluation
 
 def predict_ranking_from_group(group, movies, movie_ratings):
+    ranking = []
+    weights = np.repeat(1/len(group), len(group))
     ratings_matrix = get_ratings_matrix(group, movies, movie_ratings)
-    print(ratings_matrix)
+
+    chosen_movie, movies, ratings_matrix, weights = choose_movie_and_update_weights(
+        ratings_matrix, movies, group, weights, movie_ratings)
+
+    ranking.append(chosen_movie)
 
 def get_ratings_matrix(users, movies, movie_ratings):
     ratings_matrix = np.empty([len(users), len(movies)])
-    
+
     for i in range(0, len(users)):
         user_ratings = []
         for movie in movies:
@@ -16,3 +22,19 @@ def get_ratings_matrix(users, movies, movie_ratings):
         ratings_matrix[i] = user_ratings
 
     return ratings_matrix
+
+
+def choose_movie_and_update_weights(ratings_matrix, movies, group, weights, movie_ratings):
+    weighted_matrix = (ratings_matrix.T * weights).T
+    final_ratings = np.apply_along_axis(np.sum, 0, weighted_matrix)
+
+    chosen_movie_index = np.argsort(-final_ratings)
+    chosen_movie = movies[chosen_movie_index]
+    movies = np.delete(movies, chosen_movie_index)
+
+    ratings_matrix = np.delete(ratings_matrix, chosen_movie_index, 1)
+
+    # Update weights
+
+    return chosen_movie, movies, ratings_matrix, weights
+    
